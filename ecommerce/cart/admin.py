1ac453a1693a75b11b1e2e29adeb02c5f007ffa6
unknown <suryasureshbabu97@gmail.com>
2024-11-01 19:06:24 +0530

    added4

--- ecommerce/cart/admin.py
+++ ecommerce/cart/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.http import HttpResponse
 from cart.models import Cart,Order_details,Payment
-from django.http import HttpResponse
+
 admin.site.register(Cart)
 admin.site.register(Order_details)
 admin.site.register(Payment)
